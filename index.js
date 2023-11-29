10fb121a750fee4b3f4eaad66bb23b8de0c06b59
Ethan Clites <ethanclites@Ethans-MacBook-Air.local>
2023-11-28 21:12:51 -0500

    Rework

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+const { scrapeBearer } = require("./scrapeBearer");
+const { getActiveCalls } = require("./cadApi");
+
 const app = express()
 
 const PORT = process.env.PORT || 4000;
+var BearerToken = "erghegrirgeiu"
 
-app.get("/scrape",(req,res)=>{
-    scrapeLogic(res);
+app.get("/getbearer",(req,res)=>{
+    scrapeBearer(res);
+})
+
+app.get("/getActiveCalls",async(req,res)=>{
+    if (BearerToken==null) {await scrapeBearer({send:function(BT){BearerToken=BT}})}
+    await getActiveCalls(BearerToken,function(calls,b){
+        BearerToken = b
+        res.send(calls)
+    })
+
 })
 
 app.get('/',(req,res)=>{
--- scrapeBearer.js
+++ scrapeBearer.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
+const scrapeBearer = async (res) => {
     const browser = await puppeteer.launch({
         headless: 'new',
         args: [
@@ -45,4 +45,4 @@ const scrapeLogic = async (res) => {
       }
 }
 
-module.exports = { scrapeLogic }
+module.exports = { scrapeBearer }
